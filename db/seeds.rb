47f827d3de3eb6b396a22d0cc32a3e89f784598c
naywick <nayelawick@gmail.com>
2019-02-26 18:30:17 +0000

    created seed files and amended schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "Fashion axe marfa raw denim swag, cronut direct trade seitan franzen godard pabst. Kinfolk tousled franzen health goth scenester meggings subway tile. Vape mixtape beard poutine flexitarian small batch deep v salvia readymade tote bag. Vexillologist tbh hell of raw denim gentrify. Aesthetic godard narwhal cloud bread keytar chia kombucha ugh kogi craft beer ethical fixie cold-pressed. Health goth mumblecore af everyday carry vexillologist migas narwhal pabst hoodie paleo."
+  )
+end
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilised: 15
+  )
+end
+puts "5 skills created"
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio item #{portfolio_item}",
+    subtitle: "My great service",
+    body: "Test body text",
+    main_image: "https://via.placeholder.com/600X400",
+    thumb_image: "https://via.placeholder.com/350x200",
+  )
+end
